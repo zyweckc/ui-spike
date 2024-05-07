38b13e8cbb36de54197aa3158e6c145b954e9442
Christian Zyweck <c.zyweck@enbw.com>
2024-05-07 20:05:24 +0200

    adds detekt with default configuration and baseline for static code analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("io.gitlab.arturbosch.detekt").version("1.23.6")
 	kotlin("jvm") version "1.9.23"
 	kotlin("plugin.spring") version "1.9.23"
 }
