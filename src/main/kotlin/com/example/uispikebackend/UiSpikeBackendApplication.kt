d2f538cab252c7a4cd1601ecb396c050425a81ef
Christoph Eikermann <c.eikermann@enbw.com>
2024-05-07 14:50:13 +0200

    init spring boot

---
+++ src/main/kotlin/com/example/uispikebackend/UiSpikeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.uispikebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UiSpikeBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<UiSpikeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/uispikebackend/UiSpikeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.uispikebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UiSpikeBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
