a3456a3f84e239259d36f356c4d7e2be90c1b792
Christian Zyweck <c.zyweck@enbw.com>
2024-05-08 13:17:18 +0200

    renames UiSpikeBackendApplication to App, adds Kotlin-Logging dependency, configures logback and adds sample log entries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("io.gitlab.arturbosch.detekt").version("1.23.6")
-	id("com.avast.gradle.docker-compose") version "0.17.6"
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("io.gitlab.arturbosch.detekt").version("1.23.6")
+    id("com.avast.gradle.docker-compose") version "0.17.6"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 dockerCompose {
-	useComposeFiles = listOf("./infra/docker-compose.yml")
-	stopContainers = false
+    useComposeFiles = listOf("./infra/docker-compose.yml")
+    stopContainers = false
 }
 
 dockerCompose.isRequiredBy(tasks.named("bootRun"))
@@ -20,32 +20,33 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.security:spring-security-oauth2-jose")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.github.oshai:kotlin-logging-jvm:6.0.9")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/com/example/uispikebackend/address/adapter/outbound/AddressResourceLoader.kt
+++ src/main/kotlin/com/example/uispikebackend/address/adapter/outbound/AddressResourceLoader.kt
@@ -1,19 +1,28 @@
 package com.example.uispikebackend.address.adapter.outbound
 
 import com.example.uispikebackend.address.model.Address
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import org.springframework.stereotype.Component
 
 @Component
 class AddressResourceLoader {
-    private val addresses = List(100) { idx ->
-        Address("John $idx", "Doe", idx + 20, "j.doe@email.com")
-    }
+    private val logger = logger {}
+    private val addresses = initializeAddresses()
 
     fun loadAddress(): List<Address> {
+        logger.info { "Retrieving all addresses" }
         return addresses
     }
 
     fun loadOne(): Address {
+        logger.info { "Retrieving one address" }
         return addresses.first()
+    }
+
+    private fun initializeAddresses(): List<Address> {
+        logger.info { "Initializing addresses" }
+        return List(100) { idx ->
+            Address("John $idx", "Doe", idx + 20, "j.doe@email.com")
+        }
     }
 }
