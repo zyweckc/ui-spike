317f4467bca59a2bb39e6086cffad4cd8d22a4aa
Christian Zyweck <c.zyweck@enbw.com>
2024-05-08 12:40:33 +0200

    removes obsolete dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,13 +28,13 @@ repositories {
 }
 
 dependencies {
-	// implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.security:spring-security-oauth2-jose")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
