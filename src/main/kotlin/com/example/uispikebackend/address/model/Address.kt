700656850cf7fab81c258ce45b9724f2b0a017f2
Christian Zyweck <c.zyweck@enbw.com>
2024-05-10 20:29:17 +0200

    refactor: adds a uuid field to the address data class

--- src/main/kotlin/com/example/uispikebackend/address/model/Address.kt
+++ src/main/kotlin/com/example/uispikebackend/address/model/Address.kt
@@ -1,6 +1,9 @@
 package com.example.uispikebackend.address.model
 
+import java.util.*
+
 data class Address(
+    val id: UUID,
     var firstname: String,
     var lastname: String,
     var age: Int,
