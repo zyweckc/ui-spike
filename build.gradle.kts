9089bed5d8ec5f2e1df77d9698c4a882c23364fb
Christian Zyweck <c.zyweck@enbw.com>
2024-05-13 15:11:33 +0200

    feature: refactors security configuration to include security filters for both api and web-ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ repositories {
 dependencies {
     implementation("org.springframework.security:spring-security-oauth2-jose")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- src/main/kotlin/com/example/uispikebackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/uispikebackend/config/SecurityConfig.kt
@@ -3,6 +3,8 @@ package com.example.uispikebackend.config
 import com.example.uispikebackend.config.security.KeycloakJwtAuthenticationConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -11,25 +13,35 @@ import org.springframework.security.web.SecurityFilterChain
 @EnableWebSecurity
 class SecurityConfig {
 
+    @Order(1)
     @Bean
-    fun configure(http: HttpSecurity): SecurityFilterChain {
-        return http.authorizeHttpRequests {
-            it
-                // actuators
-                .requestMatchers("/actuator/health").permitAll()
-                // OpenAPI
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/swagger-ui.html").permitAll()
-                // domain api
-                .requestMatchers("/api/address/**").hasRole("user")
-                // web frontend
-                .requestMatchers("/ui/").permitAll()
-                .requestMatchers("/ui/details").permitAll()
-                // block all other requests
-                .anyRequest().denyAll()
-        }.oauth2ResourceServer { oauth2 ->
-            oauth2.jwt { jwt -> jwt.jwtAuthenticationConverter(KeycloakJwtAuthenticationConverter()) }
-        }.build()
+    fun apiSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .securityMatcher("/api/**")
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/api/address/**").hasRole("user")
+            }.oauth2ResourceServer { oauth2 ->
+                oauth2.jwt { jwt -> jwt.jwtAuthenticationConverter(KeycloakJwtAuthenticationConverter()) }
+            }.build()
+    }
+
+    @Bean
+    fun webSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .authorizeHttpRequests {
+                it
+                    // actuators
+                    .requestMatchers("/actuator/health").permitAll()
+                    // OpenAPI
+                    .requestMatchers("/v3/api-docs/**").permitAll()
+                    .requestMatchers("/swagger-ui/**").permitAll()
+                    .requestMatchers("/swagger-ui.html").permitAll()
+                    // web frontend
+                    .requestMatchers("/ui/").fullyAuthenticated()
+                    .requestMatchers("/ui/details").fullyAuthenticated()
+                    // block all other requests
+                    .anyRequest().denyAll()
+            }.oauth2Login(Customizer.withDefaults()).build()
     }
 }
